4eb6f37ea318752ba87826ccb5e4892cb76eaffc
UBUNTU-NETOLOGY <e.khristin@gmail.com>
2025-03-15 21:56:22 +0300

    del

--- Ans_Homew3 (Копия)/terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "all_vms" {
-  value = flatten([
-    [for i in yandex_compute_instance.centos_stream_9 : {
-      name = i.name
-      ip_external   = i.network_interface[0].nat_ip_address
-      ip_internal = i.network_interface[0].ip_address
-    }],
-  ])
-}
